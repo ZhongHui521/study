9fad9ec441773f0c033ec704a5c1254c33d5feb1
huihui <907569450@qq.com>
2016-05-31 17:47:16 +0800

    完成首页的一部分布局

--- app/src/main/java/com/huixue/study/MainActivity.java
+++ app/src/main/java/com/huixue/study/MainActivity.java
@@ -1,6 +1,7 @@
 package com.huixue.study;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RelativeLayout;
@@ -9,13 +10,20 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*设置不显示标题栏*/
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
+        /*设置不显示标题栏*/
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.hide();
+
+
+
+
+
+        /*点击底部导航栏*/
         RelativeLayout home=(RelativeLayout)findViewById(R.id.tab_main_home_layout);
         home.setOnClickListener(new View.OnClickListener(){
             @Override
